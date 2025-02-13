17c88fc312bea8aa83dae4a25986a1420a521b10
yashty <yashtyagi2612@gmail.com>
2025-02-13 17:27:01 +0530

    basic core types

---
+++ L1/app.js
@@ -0,0 +1,8 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+var n1 = 20;
+var n2 = 30;
+var ans = add(n1, n2);
+console.log(ans);
+// number, string, boolean
---
+++ L1/app.ts
@@ -0,0 +1,11 @@
+function add(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+const n1: number = 20;
+const n2: number = 30;
+
+const ans = add(n1, n2);
+console.log(ans);
+
+// number, string, boolean
